4cd71f8c75ca033e28e3661aeb1723917b363355
ken0nek <ken.for.develop@gmail.com>
2016-06-05 11:45:50 +0900

    Complete

--- CountColor/ViewController.swift
+++ CountColor/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number = 0
+    @IBOutlet weak var numberLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus() {
+        number += 1
+        handleNumber()
+    }
 
+    @IBAction func minus() {
+        number -= 1
+        handleNumber()
+    }
+
+    @IBAction func times() {
+        number *= 2
+        handleNumber()
+    }
+
+    @IBAction func divide() {
+        number /= 2
+        handleNumber()
+    }
+
+    @IBAction func clear() {
+        number = 0
+        handleNumber()
+    }
+
+    func handleNumber() {
+        numberLabel.text = "\(number)"
+
+        if number > 0 {
+            numberLabel.textColor = UIColor.blueColor()
+        } else if number == 0 {
+            numberLabel.textColor = UIColor.blackColor()
+        } else {
+            numberLabel.textColor = UIColor.redColor()
+        }
+    }
 }
 
